3d2987b2497d395c504d32371306304920874b9a
shubham526 <shubham.chatterjee94@gmail.com>
2020-04-14 09:31:11 -0400

    Added parameter for choosing optimizing criteria.

--- rank_lib_runner.py
+++ rank_lib_runner.py
@@ -3,19 +3,20 @@
 import argparse
 
 __author__ = "Shubham Chatterjee"
-__version__ = "7/11/19"
+__version__ = "4/14/20"
 
 import subprocess as sp
 import os
 import sys
 
 
-def run(rlib_path, feature_file, model_file):
+def run(rlib_path, feature_file, model_file, metric):
     if not os.path.exists(feature_file):
         print("Feature file does not exist.")
         sys.exit(-1)
 
-    rlib_command = 'java -jar {} -train {} -ranker 4 -metric2t MAP -save {}'.format(rlib_path, feature_file, model_file)
+    rlib_command = 'java -jar {} -train {} -ranker 4 -metric2t {} -save {}'.format(rlib_path, feature_file,
+                                                                                   metric, model_file)
     process = sp.Popen(rlib_command.split(), stdout=sp.PIPE)
     for line in process.stdout:
         sys.stdout.write(line.decode('utf-8'))
@@ -32,8 +33,9 @@ def main():
     parser.add_argument("--jar", help="Path to the RankLib JAR file.", required=True)
     parser.add_argument("--feature", help="Path to the RankLib compatible feature file (training data).", required=True)
     parser.add_argument("--model", help="Path to the model file.", required=True)
+    parser.add_argument("--metric", help="Metric to optimize for (MAP|NDCG@k|DCG@k|P@k|RR@k|ERR@k)", required=True)
     args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
-    run(args.jar, args.feature, args.model)
+    run(args.jar, args.feature, args.model, args.metric)
 
 
 if __name__ == '__main__':
